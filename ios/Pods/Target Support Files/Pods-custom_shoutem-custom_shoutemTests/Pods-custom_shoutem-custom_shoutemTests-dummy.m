6b327d89ec372d0b2032ebba1610f4daecdda2ab
software dev <meetnative@yandex.com>
2020-10-16 19:54:02 -0700

    - initial project commit

---
+++ ios/Pods/Target Support Files/Pods-custom_shoutem-custom_shoutemTests/Pods-custom_shoutem-custom_shoutemTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_custom_shoutem_custom_shoutemTests : NSObject
+@end
+@implementation PodsDummy_Pods_custom_shoutem_custom_shoutemTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-custom_shoutem-tvOSTests/Pods-custom_shoutem-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_custom_shoutem_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_custom_shoutem_tvOSTests
+@end
